22819ec663a89c6f2f9935909c16aa837c173b3b
elshanamazov <elshanamazov@gmail.com>
2024-02-07 00:11:49 +0400

    added transition & replaced extra code

--- src/containers/OpponentSelect/OpponentSelect.tsx
+++ src/containers/OpponentSelect/OpponentSelect.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/UI/Button/Button';
+import Rating from '../../components/UI/Rating/Rating';
 import styles from './OpponentSelect.module.scss';
 
 const OpponentSelect = () => {
@@ -13,12 +15,20 @@ const OpponentSelect = () => {
 		navigate('/duel-setting/auto');
 	};
 
+	const [rating, setRating] = useState(0);
+
+	const handleRatingChange = (newRating: number) => {
+		console.log(`Новый рейтинг: ${newRating}`);
+		setRating(newRating);
+	};
+
 	return (
 		<section className="pt-100">
 			<h2 className="title">Selection of opponents</h2>
 			<div className={styles.cta}>
 				<Button label="Manual Selection" onClick={handleManualSelect} />
 				<Button label="Automatic Selection" onClick={handleAutoSelect} />
+				<Rating rating={rating} onChange={handleRatingChange} />
 			</div>
 		</section>
 	);
