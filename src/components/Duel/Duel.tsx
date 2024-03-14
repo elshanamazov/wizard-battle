c9e98ba25b7b52898a3e28ce7e54efafacec6980
elshanamazov <elshanamazov@gmail.com>
2024-03-14 18:08:13 +0400

    add visual indicator for active player

--- src/components/Duel/Duel.tsx
+++ src/components/Duel/Duel.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
+import arrowIcon from '../../assets/icons/arrow.svg';
 import { SelectedDuelists } from '../../containers/selectedDuelists';
 import useLocalStorage from '../../hooks/useLocalStorage';
 import useWizardsData from '../../hooks/useWizardsData';
 import { SpellsResponseType, getSpells } from '../../services/spellsServices';
-import Modal from '../UI/Modal/Modal';
 import SkeletonLoader from '../UI/Skeleton/Skeleton';
 import styles from './Duel.module.scss';
 import DuelCard from './DuelCard/DuelCard';
@@ -19,9 +19,7 @@ const Duel = () => {
 	const { findWizardById, isLoading } = useWizardsData();
 	const [isSpellsLoading, setIsSpellsLoading] = useState<boolean>(true);
 	const [spells, setSpells] = useState<SpellsResponseType[]>([]);
-	const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
 	const [activePlayerId, setActivePlayerId] = useState<string | undefined>('');
-	const [modalText, setModalText] = useState<string>('');
 
 	const firstWizardDetails = selectedWizards?.firstDuelist
 		? findWizardById(selectedWizards.firstDuelist.id)
@@ -29,10 +27,6 @@ const Duel = () => {
 	const secondWizardDetails = selectedWizards?.secondDuelist
 		? findWizardById(selectedWizards.secondDuelist.id)
 		: null;
-
-	const modalHandler = (): void => {
-		setIsModalOpen(!isModalOpen);
-	};
 
 	const getRandomInt = (min: number, max: number): number => {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -42,16 +36,7 @@ const Duel = () => {
 		const activeWizardId =
 			Math.random() < 0.5 ? selectedWizards?.firstDuelist?.id : selectedWizards?.secondDuelist?.id;
 		setActivePlayerId(activeWizardId);
-
-		const activeWizardName =
-			activeWizardId === selectedWizards?.firstDuelist?.id
-				? selectedWizards?.firstDuelist?.name
-				: selectedWizards?.secondDuelist?.name;
-
-		setModalText(
-			`At the beginning of the duel, it is randomly determined that ${activeWizardName} will go first.`,
-		);
-	}, [selectedWizards, firstWizardDetails, secondWizardDetails]);
+	}, [selectedWizards]);
 
 	useEffect((): void => {
 		const loadSpells = async () => {
@@ -70,94 +55,99 @@ const Duel = () => {
 	}, []);
 
 	return (
-		<>
-			{isModalOpen && <Modal text={modalText} isOpen={true} onClose={modalHandler} />}
-			<section className={`pt-100`}>
-				<h2 className="title">Duel </h2>
-				<p className="desc">
-					Two power wizards facing each other in a fierce duel. Choose your spells wisely to win the
-					duel.
-				</p>
-				<div className={styles.duel__wrap}>
-					<div
-						className={`${styles.duel__panel} ${
-							activePlayerId === selectedWizards?.firstDuelist?.id
-								? ''
-								: styles.duel__panel_disabled
-						}`}>
-						<div className={styles.duel__header}>
-							<div className={styles.duel__potions}>
-								<DuelPotion />
+		<section className={`pt-100`}>
+			<h2 className="title">Duel </h2>
+			<p className="desc">
+				Two power wizards facing each other in a fierce duel. Choose your spells wisely to win the
+				duel.
+			</p>
+			<div className={styles.duel__wrap}>
+				<div
+					className={`${styles.duel__panel} ${
+						activePlayerId === selectedWizards?.firstDuelist?.id ? '' : styles.duel__panel_disabled
+					}`}>
+					<div className={styles.duel__header}>
+						{activePlayerId === selectedWizards?.firstDuelist?.id && (
+							<div className={styles.turnPointer}>
+								<p>Cast your spell</p>
+								<img src={arrowIcon} alt="Active player indicator" loading="lazy" />
 							</div>
-							{isLoading ? (
-								<SkeletonLoader count={1} width={360} height={320} />
-							) : (
-								<DuelCard
-									name={selectedWizards?.firstDuelist?.name}
-									health={100}
-									mana={100}
-									imagePath={firstWizardDetails?.imagePath}
-								/>
-							)}
+						)}
+						<div className={styles.duel__potions}>
+							<DuelPotion />
 						</div>
-						<div className={styles.duel__spells}>
-							{isSpellsLoading ? (
-								<SkeletonLoader count={24} width={185} height={90} />
-							) : (
-								spells.map((spell) => (
-									<DuelSpell
-										key={spell.id}
-										name={spell.name}
-										damage={getRandomInt(20, 50)}
-										manaCost={getRandomInt(10, 30)}
-									/>
-								))
-							)}
-						</div>
+						{isLoading ? (
+							<SkeletonLoader count={1} width={360} height={320} />
+						) : (
+							<DuelCard
+								name={selectedWizards?.firstDuelist?.name}
+								health={100}
+								mana={100}
+								imagePath={firstWizardDetails?.imagePath}
+							/>
+						)}
 					</div>
-					<div className={styles.duel__points}>
-						<span>Turn points:</span> 10 / 10
-					</div>
-					<div
-						className={`${styles.duel__panel} ${
-							activePlayerId === selectedWizards?.secondDuelist?.id
-								? ''
-								: styles.duel__panel_disabled
-						}`}>
-						<div className={`${styles.duel__header} ${styles.duel__header_reverse}`}>
-							<div className={styles.duel__potions}>
-								<DuelPotion />
-							</div>
-							{isLoading ? (
-								<SkeletonLoader count={1} width={360} height={320} />
-							) : (
-								<DuelCard
-									name={selectedWizards?.secondDuelist?.name}
-									health={100}
-									mana={100}
-									reverse={true}
-									imagePath={secondWizardDetails?.imagePath}
+					<div className={styles.duel__spells}>
+						{isSpellsLoading ? (
+							<SkeletonLoader count={24} width={185} height={90} />
+						) : (
+							spells.map((spell) => (
+								<DuelSpell
+									key={spell.id}
+									name={spell.name}
+									damage={getRandomInt(20, 50)}
+									manaCost={getRandomInt(10, 30)}
 								/>
-							)}
-						</div>
-						<div className={styles.duel__spells}>
-							{isSpellsLoading ? (
-								<SkeletonLoader count={24} width={185} height={90} />
-							) : (
-								spells.map((spell) => (
-									<DuelSpell
-										key={spell.id}
-										name={spell.name}
-										damage={getRandomInt(20, 50)}
-										manaCost={getRandomInt(10, 30)}
-									/>
-								))
-							)}
-						</div>
+							))
+						)}
 					</div>
 				</div>
-			</section>
-		</>
+				<div className={styles.duel__points}>
+					<span>Turn points:</span> 10 / 10
+				</div>
+				<div
+					className={`${styles.duel__panel} ${
+						activePlayerId === selectedWizards?.secondDuelist?.id ? '' : styles.duel__panel_disabled
+					}`}>
+					<div className={`${styles.duel__header} ${styles.duel__header_reverse}`}>
+						{activePlayerId === selectedWizards?.secondDuelist?.id && (
+							<div className={styles.turnPointer}>
+								<p>Cast your spell</p>
+								<img src={arrowIcon} alt="Active player indicator" loading="lazy" />
+							</div>
+						)}
+						<div className={styles.duel__potions}>
+							<DuelPotion />
+						</div>
+						{isLoading ? (
+							<SkeletonLoader count={1} width={360} height={320} />
+						) : (
+							<DuelCard
+								name={selectedWizards?.secondDuelist?.name}
+								health={100}
+								mana={100}
+								reverse={true}
+								imagePath={secondWizardDetails?.imagePath}
+							/>
+						)}
+					</div>
+					<div className={styles.duel__spells}>
+						{isSpellsLoading ? (
+							<SkeletonLoader count={24} width={185} height={90} />
+						) : (
+							spells.map((spell) => (
+								<DuelSpell
+									key={spell.id}
+									name={spell.name}
+									damage={getRandomInt(20, 50)}
+									manaCost={getRandomInt(10, 30)}
+								/>
+							))
+						)}
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
 
